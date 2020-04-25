a100e522e813639c88c1976a2d7a9024cd870ab0
Juan Gabriel Perez Guerra <perezjuang@hotmail.com>
2020-04-25 00:23:54 -0500

    Actualizar Informacion

--- demoJGP_TESTING/src/main/java/com/jgp/controller/EmployeeController.java
+++ demoJGP_TESTING/src/main/java/com/jgp/controller/EmployeeController.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,7 @@ import com.jgp.entity.Employees;
 import com.jgp.repository.EmployeeRepository;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class EmployeeController 
 {
 	@Autowired
