c59f7e479ebf8d26ef4e1f19969c82f258f3ac1b
Juan Gabriel Perez Guerra <perezjuang@hotmail.com>
2020-05-28 22:07:16 -0500

    Docker

---
+++ JenkinsfileJGP.groovy
@@ -0,0 +1,191 @@
+pipeline {
+   agent any
+          /* https://www.journaldev.com/33645/maven-commands-options-cheat-sheet  */
+    environment {
+        gitURL = 'https://perezjuang:86100252807aA*@github.com/perezjuang/gabriel-coding-tips.git'
+        branchDEV = 'dev'
+        branchQA= 'qa'
+        branchPRD = 'master'
+    }
+
+   stages {
+      stage('GET-CODE') {
+         steps { 
+            echo 'GET-CODE================================================'
+           catchError(buildResult: 'SUCCESS', stageResult: 'SUCCESS')
+            {
+                        bat  """
+                              mkdir DEV
+                             """.stripIndent().trim()
+
+            }
+                                         
+                        dir ('DEV') {  
+                           git branch: env.branchPRD, url: env.gitURL
+                            
+                        }
+
+         }
+      }
+      
+      
+      
+      
+    stage('TEST') {
+         steps {
+            echo 'TEST ============================================'
+            dir ('DEV') { 
+                   dir ('demoJGPDevSecOps') { 
+                        bat  """
+                        mvn verify
+                        """.stripIndent().trim()
+                    }
+                
+                }
+
+         }
+      }
+      
+      
+    stage('COMPILE') {
+         steps {
+            echo '====================================================='
+            echo 'COMPILE'
+            echo '====================================================='
+            dir ('DEV') { 
+                dir ('demoJGPDevSecOps') { 
+                    bat  """
+                      mvn package
+                      mvn install
+                     """.stripIndent().trim()
+                }
+            }
+
+         }
+      }
+      
+      
+stage('GENERATE_ARTIFACTS') {
+            steps {
+                
+                parallel("GENERATE_ARTIFACTS_GIT": {
+                      //git branch: env.branchPRD, url: env.gitURL
+            dir ('DEV') { 
+                   dir ('demoJGPDevSecOps') { 
+                        //bat "git tag -a some_tag -m 'Jenkins'"
+                        //bat "git push " + env.gitURL + " --tags"
+                        
+                        script {
+/*def pom = new File('pom.xml').getText('utf-8')
+def doc = new XmlParser().parseText(pom)
+def version = doc.version.text()
+                           //def pom = new XmlParser().parse(new File('C:\\Program Files (x86)\\Jenkins\\workspace\\devopsTest\\DEV\\demoJGPDevSecOps\\pom.xml'))
+                           println(version)
+                           //pom.children().each( { it.children().each( { println it }) } )*/
+                           
+                           //================================================================
+                           /*
+                           println "Release Script Start -----"
+
+def dir = new File('pom.xml')..getText('utf-8')
+// copy a backup of the pom
+println "A copy of the old pom is saved in pom.backup.xml"
+ant.copy(file: dir, tofile: "pom.backup.xml")
+
+def pom = new XmlSlurper().parse(dir)
+
+// increase version number
+def version = pom.version.toString().replace("-SNAPSHOT", "").split("\\.")
+version[-1] = version[-1].toInteger()+1
+println "Previoues version of pom: ${pom.version}"
+pom.version = version.join('.') 
+println "New Version of pom: ${pom.version}"
+
+
+// remove snapshots in properties
+print "remove snapshots in properties "
+pom.properties.childNodes().each{
+	it.replaceBody(it.text().replace("-SNAPSHOT", ""))		
+}
+println "OK"
+
+// remove snapshots in dependencies
+print "remove snapshots in dependencies "
+pom.dependencies.dependency.each{
+	it.version.replaceBody(it.version.text().replace("-SNAPSHOT", ""))		
+}
+println "OK"
+
+
+
+// output the pom
+print "Writing new pom.xml "
+
+def writer = dir.newWriter()
+writer << XmlUtil.serialize(result)
+writer.close()
+println "OK"
+println "Release Script End -----"
+                           
+                           //==================================================================
+                         */  
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                       }
+                   }
+            }
+                      
+                },
+                
+                "GENERATE_ARTIFACTS_BACKUP": {
+                            echo "world"
+                        }
+                )
+            }
+        }
+
+
+
+
+
+
+  /*
+  sh 'git tag -a tagName -m "Your tag comment"'
+sh 'git merge develop'
+sh 'git commit -am "Merged develop branch to master'
+sh "git push origin master"
+
+   bat('git push https://perezjuang:86100252807aA*@github.com/perezjuang/gabriel-coding-tips.git --tags')
+
+*//*
+ bat 'git tag https://perezjuang:86100252807aA*@github.com/perezjuang/gabriel-coding-tips.git -a tagName -m "Comentario01"'
+ bat 'git merge https://perezjuang:86100252807aA*@github.com/perezjuang/gabriel-coding-tips.git master'
+ bat 'git commit https://perezjuang:86100252807aA*@github.com/perezjuang/gabriel-coding-tips.git  -am "Merged Master branch to master'
+ bat "git push https://perezjuang:86100252807aA*@github.com/perezjuang/gabriel-coding-tips.git origin master"
+*/
+
+          
+         //}
+      }
+      
+      
+      
+   }
--- demoJGPDevSecOps/src/main/java/com/jgp/SpringBootDemoApplication.java
+++ demoJGPDevSecOps/src/main/java/com/jgp/SpringBootDemoApplication.java
@@ -1,9 +1,11 @@
 package com.jgp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan; 
 
 @SpringBootApplication 
+@ComponentScan(basePackages={"com.app"})
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
