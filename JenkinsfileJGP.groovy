07d8c9f742231b05a3755f5ded6e37d72795542f
Juan Gabriel Perez Guerra <perezjuang@hotmail.com>
2020-08-15 00:23:03 -0500

    Update JenkinsfileJGP.groovy

--- JenkinsfileJGP.groovy
+++ JenkinsfileJGP.groovy
@@ -1,39 +1,31 @@
-pipeline {
-    agent none
-    stages {
-        stage('Run Tests') {
-            parallel {
-                stage('Test On Windows') {
-                    agent {
-                        label "windows"
-                    }
-                    steps {
-                        bat "run-tests.bat"
-                    }
-                    post {
-                        always {
-                            junit "**/TEST-*.xml"
-                        }
-                    }
-                }
-                stage('Test On Linux') {
-                    agent {
-                        label "linux"
-                    }
-                    steps {
-                        sh "run-tests.sh"
-                    }
-                    post {
-                        always {
-                            junit "**/TEST-*.xml"
-                        }
-                    }
-                }
-            }
-        }
+node {
+
+    stage('Preparation') { // for display purposes
+
     }
+    stage('Build') {
+     
+    }
+    stage('Results') {
+
+    }
+    stage('run-parallel-branches') {
+        steps {
+            parallel(
+            a: {
+                echo "This is branch a"
+            },
+            b: {
+                echo "This is branch b"
+            }
+            )
+        }
 }
 
+
+
+
+}
 
 
 
