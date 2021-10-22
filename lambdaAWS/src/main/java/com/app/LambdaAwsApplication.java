602a1133029b16d9cc9953a9ec9fe4abd2e0550f
Juan Gabriel <Perezjuang@hotmail.com>
2021-10-22 15:31:08 -0500

    update

--- lambdaAWS/src/main/java/com/app/LambdaAwsApplication.java
+++ lambdaAWS/src/main/java/com/app/LambdaAwsApplication.java
@@ -1,13 +1,21 @@
 package com.app;
 
+import java.util.function.Function;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LambdaAwsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LambdaAwsApplication.class, args);
+	}
+	
+	@Bean
+	public Function<String, String> uppercase() {
+		return value -> value.toUpperCase();
 	}
 
 }
